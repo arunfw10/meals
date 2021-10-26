b8af190f53ae92be5f8be144454aebce005227e1
arunfw10 <86434231+arunfw10@users.noreply.github.com>
2021-10-26 18:25:57 +0530

    Add files via upload

---
+++ meals/script/navbar.js
@@ -0,0 +1,25 @@
+function navbar() {
+    return ` <div id="navbar">
+    <h3>
+        <a href="">Home</a>
+        <img src="https://www.themealdb.com/images/logo-small.png">
+    </h3>
+    <div id="options">
+     <h3>
+         <a href="">Reciepe of the day</a>
+     </h3>
+     <h3>
+         <a href="">latest Reciepe </a>
+     </h3>
+     <h3>
+         <a href="">Register</a>
+     </h3>
+     <h3>
+         <a href="">Login</a>
+     </h3>
+    </div>
+   
+    </div>
+    `;
+}
+export default navbar ;
